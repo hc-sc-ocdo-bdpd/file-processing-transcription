678d0f6de810f748307494abafaa4fce387b14a3
Brennan Cruse <brennan.cruse@hc-sc.gc.ca>
2024-09-16 12:35:47 -0400

    update req versioning

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'torch==2.3.1',
-        'openai-whisper==1.1.10',
+        'openai-whisper==20231117',
         'pytest==8.3.3',
         'numpy==1.26.4',
         'file-processing-test-data @ git+https://github.com/hc-sc-ocdo-bdpd/file-processing-test-data.git@main'
